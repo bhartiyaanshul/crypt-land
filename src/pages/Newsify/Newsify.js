9f3c0136fda699ba239280815dbe36910889e96b
Anshul Bhartiya <126051112+bhartiyaanshul@users.noreply.github.com>
2024-05-07 18:58:43 +0530

    Newsify: Features & Services section added

--- src/pages/Newsify/Newsify.js
+++ src/pages/Newsify/Newsify.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Header } from '../../components/Newsify/Header/Header'
 import { Navbar } from '../../components/Newsify/Navbar/Navbar'
 import { IntroSection } from '../../components/Newsify/IntroSection/IntroSection'
+import { Features } from '../../components/Newsify/Features/Features'
+import { ServicesSection } from '../../components/Newsify/ServicesSection/ServicesSection'
 
 export const Newsify = () => {
   return (
@@ -9,6 +11,8 @@ export const Newsify = () => {
         <Header />
         <Navbar />
         <IntroSection/>
+        <Features/>
+        <ServicesSection />
     </div>
   )
 }
